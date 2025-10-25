8833b34da579b461381cf9369738542916d9a8c6
maxdokukin <tools.shiny.01@icloud.com>
2025-10-25 00:22:41 -0700

    init

---
+++ haaangry-frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  haaangry-frontend
+//
+//  Created by xewe on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ haaangry-frontend/haaangry_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  haaangry_frontendApp.swift
+//  haaangry-frontend
+//
+//  Created by xewe on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct haaangry_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
